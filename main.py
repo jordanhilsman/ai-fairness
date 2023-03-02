cf0e3d67b09e39f971d752eb3404d3d10fc7d7ed
Jordan <113873023+jordanhilsman@users.noreply.github.com>
2023-03-02 15:22:04 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # import libraries
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from proportional_parity import *
@@ -21,15 +22,17 @@ X['marital-status'] = [1 if x == ' Married-civ-spouse' else 0 for x in df['marit
 X['native-country'] = [1 if x == ' United States' else 0 for x in df['native-country']]
 
 occ_groups = {
-        ' Priv-house-serv': 0, ' Other-service': 0, ' Handlers-cleaners': 0,
-        ' Farming-fishing': 1, ' Machine-op-inspct': 1, ' Adm-clerical': 1,
-        ' Transport-moving': 2, ' Craft-repair': 2, ' Sales': 2,
-        ' Armed-Forces': 3, ' Tech-support': 3, ' Protective-serv': 3,
-        ' Prof-specialty': 4, ' Exec-managerial': 4}
+        ' Priv-house-serv': 0, ' Other-service': 1, ' Handlers-cleaners': 2,
+        ' Farming-fishing': 3, ' Machine-op-inspct': 4, ' Adm-clerical': 5,
+        ' Transport-moving': 6, ' Craft-repair': 7, ' Sales': 8,
+        ' Armed-Forces': 9, ' Tech-support': 10, ' Protective-serv': 11,
+        ' Prof-specialty': 12, ' Exec-managerial': 13}
 
 race = {
         ' White': 0, ' Black': 1, ' Asian-Pac-Islander': 2, ' Amer-Indian-Eskimo': 3, ' Other': 4
 }
+
+
 
 X['occupation'] = [occ_groups[x] for x in df['occupation']]
 X['race'] = [race[x] for x in df['race']]
@@ -39,13 +42,14 @@ no_target = X.loc[:, X.columns != "target"]
 X['target'] = target
 
 # Perform train/test split for predictions and assessment.
-X_train, X_test, y_train, y_test = train_test_split(no_target, target, test_size=0.3, random_state=3)
+X_train, X_test, y_train, y_test = train_test_split(no_target, target, test_size=0.1, random_state=3)
 model = LogisticRegression()
 
 model.fit(X_train, y_train)
 # Predict outcomes with the model
 y_preds = model.predict(X_test)
-
 # Execute proportional parity test - In this case, we are examining on the feature 'race', with ' White'
 # set as the baseline.
-prop_parity(X_test, y_test, df['race'], ' White', y_preds)
+prop_parity(X_test, y_test, race, ' Black', y_preds, 'race')
+prop_parity(X_test, y_test, occ_groups, ' Armed-Forces', y_preds, 'occupation')
+
