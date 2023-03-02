28880cac5958f4973905152097dcfe2590052196
Jordan <113873023+jordanhilsman@users.noreply.github.com>
2023-03-02 15:21:32 -0500

    Add files via upload

--- proportional_parity.py
+++ proportional_parity.py
@@ -15,7 +15,7 @@ in numbers lower than 1.
 Parameters:
 data: input dataframe with necessary columns
 outcome: column name that has outcome variable (target column)
-group: Column name indicating group to evaluation proportional parity on.
+group: Dictionary corresponding to the feature you'd like to evaluate proportional parity on.
 base: Reference group for proportional parity.
 predictions: predicted outcome vector
 '''
@@ -24,37 +24,57 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 
 
-def prop_parity(data, outcome, group, base, predictions):
+def prop_parity(data, outcome, group, base, predictions, col_name):
 
     # first check if data is a dataframe object
     if isinstance(data, pd.DataFrame):
         pass
     else:
         data = data.to_frame()
+
+    # initialize accuracy array, where proportional parity and accuracy will be reported.
     accuracy_array = []
+
     # check lengths
     if len(outcome) != len(predictions):
         print('Prediction vector and outcome vector must be of the same length!')
     else:
         pass
-    group_break = group.unique()
+
+    # get unique names for the specified group and create a dictionary for iteration
+    group = pd.DataFrame(group.items())
+    group_break = group[0].unique()
     dictionary = dict(list(enumerate(group_break)))
     dictionary = {v: k for k, v in dictionary.items()}
+    # append test outcomes and predicted outcomes to our data for accuracy computation.
+    # iteratively run through the dataset for each
     data['y_test'] = outcome
     data['y_preds'] = predictions
     for x in dictionary.values():
-        final_df = data[data['race'] == x]
-        race = group_break[x]
-        acc = accuracy_score(final_df['y_test'], final_df['y_preds'])
-        print('The accuracy for the ' + race + " subset is ", acc)
-        accuracy_array.append(
-            {
-                'Group': group_break[x],
-                'Accuracy': accuracy_score(final_df['y_test'], final_df['y_preds'])
-            }
-        )
+        # item = group_break[x]
+        if col_name == 'race':
+            final_df = data[data['race'] == x]
+#            acc = accuracy_score(final_df['y_test'], final_df['y_preds'])
+#            print('The accuracy for the ' + item + " subset is ", acc)
+            accuracy_array.append(
+                {
+                    'Group': group_break[x],
+                    'Accuracy': accuracy_score(final_df['y_test'], final_df['y_preds'])
+                }
+            )
+        if col_name == 'occupation':
+            final_df = data[data['occupation'] == x]
+#            acc = accuracy_score(final_df['y_test'], final_df['y_preds'])
+#            print('The accuracy for the ' + item + " subset is ", acc)
+            accuracy_array.append(
+                {
+                    'Group': group_break[x],
+                    'Accuracy': accuracy_score(final_df['y_test'], final_df['y_preds'])
+                }
+            )
+
     accuracy_array = pd.DataFrame(accuracy_array)
     baseline = base
-    baseline_accuracy = accuracy_array[accuracy_array['Group']==(baseline)]['Accuracy'].item()
-    accuracy_array['Proportional Parity'] = (accuracy_array['Accuracy'] / baseline_accuracy)
+    baseline_accuracy = accuracy_array[accuracy_array['Group'] == baseline]['Accuracy'].item()
+    accuracy_array['Proportional Parity'] = accuracy_array['Accuracy'] / baseline_accuracy
     print(accuracy_array)
