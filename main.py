c4ebcc0bd23243e01c48e3fac5fa17272fd2e630
Jordan <113873023+jordanhilsman@users.noreply.github.com>
2023-03-26 01:01:11 -0400

    Add files via upload
    Optimization of data preprocessing example, proportional parity cleaned to be more applicable.

--- main.py
+++ main.py
@@ -5,83 +5,109 @@ from proportional_parity import *
 from statistical_parity_diff import *
 from disparate_impact import *
 from equal_opportunity_difference import *
-names = ['age', 'workclass', 'fnlwgt', 'education', 'education-num',
-        'marital-status', 'occupation', 'relationship', 'race', 'sex',
-        'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
-        'target']
+from average_absolute_odds_difference import *
+import numpy as np
+
+names = [
+    "age",
+    "workclass",
+    "fnlwgt",
+    "education",
+    "education-num",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "capital-gain",
+    "capital-loss",
+    "hours-per-week",
+    "native-country",
+    "target",
+]
 
 # load dataset
-df = pd.read_csv(r'C:\Users\Jordan\Downloads\adult.csv', names=names, na_values=' ?')
+df = pd.read_csv(
+    r"~/Documents/pittnail/ai-fairness-main/adult.csv", names=names, na_values=" ?"
+)
 df = df.dropna()
+df["race"] = df["race"].str.strip()
+df["marital-status"] = df["marital-status"].str.strip()
+df["native-country"] = df["native-country"].str.strip()
+df["target"] = df["target"].str.strip()
+df["occupation"] = df["occupation"].str.strip()
 
 # targets
-target = [1 if tar == ' >50K' else 0 for tar in df['target']]
+target = [1 if tar == " >50K" else 0 for tar in df["target"]]
+
 
 # features, dummy variables for logistic regression
-X = df[['age', 'education-num', 'hours-per-week']]
-X['marital-status'] = [1 if x == ' Married-civ-spouse' else 0 for x in df['marital-status']]
-X['native-country'] = [1 if x == ' United States' else 0 for x in df['native-country']]
+df["marital-status"] = np.where(df["marital-status"] == "Married-civ-spouse", 1, 0)
+df["native-country"] = np.where(df["native-country"] == "United States", 1, 0)
+df["target"] = np.where(df["target"] == ">50K", 1, 0)
 
+# perform data-bias early.
+statistical_parity_diff(df, "race", "target")
+disparate_impact(df, "race", "target")
 occ_groups = {
-        ' Priv-house-serv': 0, ' Other-service': 1, ' Handlers-cleaners': 2,
-        ' Farming-fishing': 3, ' Machine-op-inspct': 4, ' Adm-clerical': 5,
-        ' Transport-moving': 6, ' Craft-repair': 7, ' Sales': 8,
-        ' Armed-Forces': 9, ' Tech-support': 10, ' Protective-serv': 11,
-        ' Prof-specialty': 12, ' Exec-managerial': 13}
-
-race = {
-        ' White': 0, ' Black': 1, ' Asian-Pac-Islander': 2, ' Amer-Indian-Eskimo': 3, ' Other': 4
+    "Priv-house-serv": 0,
+    "Other-service": 1,
+    "Handlers-cleaners": 2,
+    "Farming-fishing": 3,
+    "Machine-op-inspct": 4,
+    "Adm-clerical": 5,
+    "Transport-moving": 6,
+    "Craft-repair": 7,
+    "Sales": 8,
+    "Armed-Forces": 9,
+    "Tech-support": 10,
+    "Protective-serv": 11,
+    "Prof-specialty": 12,
+    "Exec-managerial": 13,
 }
 
+race = {
+    "White": 0,
+    "Black": 1,
+    "Asian-Pac-Islander": 2,
+    "Amer-Indian-Eskimo": 3,
+    "Other": 4,
+}
 
+df["occupation"] = [occ_groups[x] for x in df["occupation"]]
+df["race"] = [race[x] for x in df["race"]]
+X = df[
+    [
+        "age",
+        "education-num",
+        "hours-per-week",
+        "marital-status",
+        "native-country",
+        "occupation",
+        "race",
+        "target",
+    ]
+]
 
-X['occupation'] = [occ_groups[x] for x in df['occupation']]
-X['race'] = [race[x] for x in df['race']]
 
 # created dataframe without the target column for ease in Proportional Parity library.
 no_target = X.loc[:, X.columns != "target"]
-X['target'] = target
 
 # Perform train/test split for predictions and assessment.
-X_train, X_test, y_train, y_test = train_test_split(no_target, target, test_size=0.2, random_state=3)
+X_train, X_test, y_train, y_test = train_test_split(
+    no_target, X["target"], test_size=0.2, random_state=3
+)
 model = LogisticRegression()
 
 model.fit(X_train, y_train)
 # Predict outcomes with the model
 y_preds = model.predict(X_test)
-# Execute proportional parity test - In this case, we are examining on the feature 'race', with ' White'
-# set as the baseline.
-#prop_parity(X_test, y_test, race, ' Black', y_preds, 'race')
-#prop_parity(X_test, y_test, occ_groups, ' Adm-clerical', y_preds, 'occupation')
-
-#Statistical_Parity_Diff(df, 'race', 'target')
 
 dff = df
-dff['target'] = target
-#print(dff['target'].value_counts()) #30162
-#print(dff['race'].value_counts()) # White - 25933, Black - 2817, API - 895, AIE-286, Other-231
-#print(dff['race'].value_counts().sum()) #30162
-df2 = df.loc[(dff['race'] == ' Black') & (dff['target'] == 1)]
-#print(df2)
-#print(len(dff.loc[(dff['race'] == ' Black') & (dff['target'] == 1)]))
-#print(list(df['race'].unique()))
-#30162
-
-#print(len(dff.loc[(dff['race'] == ' White') & (dff['target'] == 1)]))
-#print(len(dff.loc[(dff['race'] == ' Black') & (dff['target'] == 1)]))
-#print(len(dff.loc[(dff['race'] == ' Asian-Pac-Islander') & (dff['target'] == 1)]))
-#print(len(dff.loc[(dff['race'] == ' Amer-Indian-Eskimo') & (dff['target'] == 1)]))
-#print(len(dff.loc[(dff['race'] == ' Other') & (dff['target'] == 1)]))
-
-#print(len(dff.loc[(dff['race'] == ' White')]))
-#print(len(dff.loc[(dff['race'] == ' Black')]))
-#print(len(dff.loc[(dff['race'] == ' Asian-Pac-Islander')]))
-#print(len(dff.loc[(dff['race'] == ' Amer-Indian-Eskimo')]))
-#print(len(dff.loc[(dff['race'] == ' Other')]))
+dff["target"] = target
+df2 = df.loc[(dff["race"] == "Black") & (dff["target"] == 1)]
 
 
-
-
-Statistical_Parity_Diff(df, 'race', 'target')
-Disparate_Impact(df, 'race', 'target', ' Black')
-equal_opportunity_difference(X_test, y_test, y_preds, 'race', 0, 1)
+equal_opportunity_difference(X_test, y_test, y_preds, "race", 0, 1)
+prop_parity(X_test, y_test, race, "White", y_preds, "race")
+average_absolute_odds_difference(X_test, y_test, y_preds, "race")
--- proportional_parity.py
+++ proportional_parity.py
@@ -27,13 +27,12 @@ def prop_parity(data, outcome, group, base, predictions, col_name):
     # first check if data is a dataframe object
     if not isinstance(data, pd.DataFrame):
         data = data.to_frame()
-
     # initialize accuracy array, where proportional parity and accuracy will be reported.
     accuracy_array = []
 
     # check lengths
     if len(outcome) != len(predictions):
-        print('Prediction vector and outcome vector must be of the same length!')
+        print("Prediction vector and outcome vector must be of the same length!")
 
     # get unique names for the specified group and create a dictionary for iteration
     group = pd.DataFrame(group.items())
@@ -41,28 +40,25 @@ def prop_parity(data, outcome, group, base, predictions, col_name):
     dictionary = dict(list(enumerate(group_break)))
     dictionary = {v: k for k, v in dictionary.items()}
     # append test outcomes and predicted outcomes to our data for accuracy computation.
-    data['y_test'] = outcome
-    data['y_preds'] = predictions
-    for x in dictionary.values():
-        if col_name == 'race':
-            final_df = data[data['race'] == x]
-            accuracy_array.append(
-                {
-                    'Group': group_break[x],
-                    'Accuracy': accuracy_score(final_df['y_test'], final_df['y_preds'])
-                }
-            )
-        if col_name == 'occupation':
-            final_df = data[data['occupation'] == x]
-            accuracy_array.append(
-                {
-                    'Group': group_break[x],
-                    'Accuracy': accuracy_score(final_df['y_test'], final_df['y_preds'])
-                }
-            )
+    data["y_test"] = outcome
+    data["y_preds"] = predictions
+    for x in group_break:
+        final_df = data[data[col_name] == dictionary[x]]
+        accuracy_array.append(
+        {
+            "Group": x,
+            "Accuracy": accuracy_score(final_df["y_test"], final_df["y_preds"]),
+         }
+         )
+
 
     accuracy_array = pd.DataFrame(accuracy_array)
     baseline = base
-    baseline_accuracy = accuracy_array[accuracy_array['Group'] == baseline]['Accuracy'].item()
-    accuracy_array['Proportional Parity'] = accuracy_array['Accuracy'] / baseline_accuracy
+    baseline_accuracy = accuracy_array[accuracy_array["Group"] == baseline][
+        "Accuracy"
+    ].item()
+    accuracy_array["Proportional Parity"] = (
+        accuracy_array["Accuracy"] / baseline_accuracy
+    )
     print(accuracy_array)
+
